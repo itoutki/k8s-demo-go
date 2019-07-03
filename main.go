0ff0b17b20e7c104dda09d77942d387bf8f8840a
itoutki <itoutki@nttdata.co.jp>
2019-07-03 11:36:07 +0900

    update

--- main.go
+++ main.go
@@ -8,13 +8,19 @@ import(
 
 func main() {
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "hello world")
+    fmt.Fprintln(w, "hello world")
   })
   http.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, os.Getenv("MY_LABEL_VERSION"))
+    fmt.Fprintln(w, os.Getenv("MY_LABEL_VERSION"))
+  })
+  http.HandleFunc("/nodename", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, os.Getenv("MY_NODE_NAME"))
+  })
+  http.HandleFunc("/name", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, os.Getenv("MY_POD_NAME"))
   })
   http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "ok")
+    fmt.Fprintln(w, "ok")
   })
   
   http.ListenAndServe(":8080", nil)
