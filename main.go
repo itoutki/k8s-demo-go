306f55e36326d7a1cdf7ed1be6491c67719b19f7
itoutki <itoutki@nttdata.co.jp>
2019-07-03 11:48:49 +0900

    update

--- main.go
+++ main.go
@@ -1,28 +1,33 @@
 package main
 
-import(
-  "os"
-  "fmt"
-  "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
 )
 
 func main() {
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "hello world")
-  })
-  http.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, os.Getenv("MY_LABEL_VERSION"))
-  })
-  http.HandleFunc("/nodename", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, os.Getenv("MY_NODE_NAME"))
-  })
-  http.HandleFunc("/name", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, os.Getenv("MY_POD_NAME"))
-  })
-  http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "ok")
-  })
-  
-  http.ListenAndServe(":8080", nil)
-}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "hello world")
+	})
+	http.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, os.Getenv("MY_LABEL_VERSION"))
+	})
+	http.HandleFunc("/nodename", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, os.Getenv("MY_NODE_NAME"))
+	})
+	http.HandleFunc("/name", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, os.Getenv("MY_POD_NAME"))
+	})
+	http.HandleFunc("/info", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "{\"version\": \"%s\", \"nodename\": \"%s\", \"nodename\": \"%s\"}\n",
+			os.Getenv("MY_LABEL_VERSION"),
+			os.Getenv("MY_NODE_NAME"),
+			os.Getenv("MY_POD_NAME"))
+	})
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "ok")
+	})
 
+	http.ListenAndServe(":8080", nil)
+}
